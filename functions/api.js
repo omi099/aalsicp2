0287fa27f9e6a85432d57cc33ce6bec12fc5764d
omi099 <76110622+omi099@users.noreply.github.com>
2024-02-10 10:08:35 +0530

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -157,5 +157,5 @@ const generateNewToken = async () => {
 //   console.log(`Server is running on port ${PORT}`);
 // });
 
-app.use('/.netlify/functions/', router);
+app.use('/.netlify/functions/api', router);
 module.exports.handler = serverless(app);
