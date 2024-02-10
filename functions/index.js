2367c111628cfbce47db5008eb5129d02d4c9afd
omi099 <76110622+omi099@users.noreply.github.com>
2024-02-10 12:17:06 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // app.set("views", path.join(__dirname, "views"));
 
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, '/.netlify/views'))
 app.set("view engine", "ejs");
 app.engine("ejs", require("ejs").__express);
 
