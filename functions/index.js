ce44093102fd5b8d63f9fbf847aa8983a9a607b9
omi099 <76110622+omi099@users.noreply.github.com>
2024-02-10 11:55:27 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -5,8 +5,7 @@ const fs = require("fs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const serverless = require('serverless-http');
-const router = express.Router()
-const ejs = require('ejs')
+const router = express.Router();
 
 const app = express();
 
